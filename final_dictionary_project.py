d833fbc67c0690a1526327a948d105653e9b3efa
Michael Moore <mmoore5325@gmail.com>
2017-11-06 20:15:34 -0500

    counting number of -classes- for each teacher, counting correctly, now have to save and return

--- final_dictionary_project.py
+++ final_dictionary_project.py
@@ -25,7 +25,29 @@ def courses(dictionary):
       courses.append(value)
       
   print(courses)
+  
+def most_courses(dictionary):
+  number_of_courses = dict()
+  for key in dictionary.keys():
+    if isinstance(dictionary[key], list):
+      print(key, len(dictionary[key]))
+    else:
+      print(key, "1")
+  #  for key in dictionary.keys():
+#      number_of_courses[key] = 
+#  for key in dictionary.keys():
+#    if key in number_of_courses[key]
+#      number_of_courses[key] = len(dictionary[key])
+#      print(number_of_courses)
+#    for value in dictionary.values():
+#      print(key, value)
+#      if isinstance(value, list):
+#        for classes in value:
+#          number_of_courses[key] += 1
+#      else:
+#        number_of_courses[key] += 1
+#        
+  print(number_of_courses)
 dictionary = {"a": ["python", "cyclops"], "b":"c++", "c":["c", "e", "f"], "d":"ruby", "e":"javascript"}
 teachers(dictionary)
 num_courses(dictionary)
-courses(dictionary)
