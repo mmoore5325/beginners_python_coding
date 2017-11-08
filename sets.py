60323521f9fa1dd25c9c8ec5023be1e4ac57479c
mike <mmoore5325@gmail.com>
2017-11-08 18:50:57 -0500

    Beginning sets in python, first is overlapping/intersecting(variable.intersection(set))

---
+++ sets.py
@@ -0,0 +1,22 @@
+COURSES = {
+    "Python Basics": {"Python", "functions", "variables",
+                      "booleans", "integers", "floats",
+                      "arrays", "strings", "exceptions",
+                      "conditions", "input", "loops"},
+    "Java Basics": {"Java", "strings", "variables",
+                    "input", "exceptions", "integers",
+                    "booleans", "loops"},
+    "PHP Basics": {"PHP", "variables", "conditions",
+                   "integers", "floats", "strings",
+                   "booleans", "HTML"},
+    "Ruby Basics": {"Ruby", "strings", "floats",
+                    "integers", "conditions",
+                    "functions", "input"}
+}
+
+def covers(topics):
+    course = []
+    for key, value in COURSES.items():
+        if value.intersection(topics):
+            course.append(key)
+    return(course)
