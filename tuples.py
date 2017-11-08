fc0c7f5574776fa86ac94d838bff9ec42bce36fd
mike <mmoore5325@gmail.com>
2017-11-08 17:05:14 -0500

    tuples exercise corrected - returns product of all numbers

--- tuples.py
+++ tuples.py
@@ -1,14 +1,13 @@
 def multiply(*nums):
-  store = []
-  for num in nums:
-    store.append(num)
-    print(store)
-  print(len(store))
-  i = 2
-  total = store[i] * store[i+1]
-  while i < len(store):
-    total *= store[i]
-    i+=1
-    print(total)
-    
-multiply(2,2,2,2,2)
+    store = []
+    for num in nums:
+        store.append(num)
+        
+    i = 2
+    total = store[0] * store[1]
+    while i < len(store):
+        total *= store[i]
+        i+=1
+
+    return(total)
+multiply(1,2,3,4,5)
