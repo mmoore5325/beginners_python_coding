5f48d0cfa45a65442ef402712335160d2e29ac88
mike <mmoore5325@gmail.com>
2017-11-08 19:44:08 -0500

    Grid creation, not my code

--- dungeongame.py
+++ dungeongame.py
@@ -8,3 +8,25 @@ import random
 # move player unless move is not valid
 # check for win or loss
 # clear screen and redraw grid on every move
+
+def gridSize_def():
+   global gridSize
+   while True:
+       try:
+           gridSize = int(input("input grid length and width\n"))
+           if gridSize<2:
+               print ("Too Small")
+           else:
+               break
+       except ValueError:
+           print ("invalid input") 
+   grids = [[] for _ in range (gridSize)]
+   for i in range(0,gridSize):
+       grids[i-1]=[0 for _ in range(gridSize)]
+   return grids
+
+def grid_def():
+ x = []
+ for i in range (0,gridSize):
+   x.append(grids[i-1])      
+ print(x)
