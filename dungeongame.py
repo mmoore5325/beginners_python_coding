2c93b2f25bddc1f9ba9b002b9aee5b3001a794b9
mike <mmoore5325@gmail.com>
2017-11-08 19:12:56 -0500

    Beginning of final project involving, sets, dicts, tuples, and other things in the section

---
+++ dungeongame.py
@@ -0,0 +1,10 @@
+import random
+
+# draw a grid(4x4, 5x5)
+# pick a random location for player
+# pick a random location for exit door
+# draw a player in grid
+# take input for movement
+# move player unless move is not valid
+# check for win or loss
+# clear screen and redraw grid on every move
